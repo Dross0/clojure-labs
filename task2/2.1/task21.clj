722572ebf43169e223be430f43c51d100ed5d293
Андрей Гайдамака <andrew.gaidamaka@gmail.com>
2022-12-12 11:59:02 +0700

    Fix memoize 2.1

--- task2/2.1/task21.clj
+++ task2/2.1/task21.clj
@@ -2,47 +2,48 @@
   (* (/ (+ (f a) (f (+ a step))) 2) step)
 )
 
+(defn integrate [f, b, step]
+  (if (<= b 0)
+      0
+      (+ (next-integral-sum-element f (- b step) step) (integrate f (- b step) step))
+  )
+)
+
 (defn integral [f, step]
   (fn [b]
-    (reduce + 
-      (map (fn [a_i] (next-integral-sum-element f a_i step))
-        (range 0 b step)
-      )
-    )
+    (integrate f b step)
   )
 )
 
 (defn integral-with-memoize [f, step]
-  (with-local-vars
-      [integrate-with-memoize (memoize 
-               (fn [b] 
-                 (reduce + 
-                    (map (fn [a_i] (next-integral-sum-element f a_i step))
-                      (range 0 b step)
-                    )
-                 )
-              )
-          )
-      ]
-    (.bindRoot integrate-with-memoize @integrate-with-memoize)
-    @integrate-with-memoize)
+  (let [integrateFunc (fn [selfFunc b]
+                 (if (<= b 0)
+                   0
+                   (+ (next-integral-sum-element f (- b step) step) (selfFunc selfFunc (- b step)))
+                ))
+        ]
+    (partial integrateFunc (memoize integrateFunc))
+  )
 )
-
 
 (defn sqr [x]
   (* x x)
 )
 
 (let [
-      simpleSqrIntegral (integral sqr 0.1),
-      memoizeSqrIntegral (integral-with-memoize sqr 0.1)
+      simpleSqrIntegral (integral sqr 1),
+      memoizeSqrIntegral (integral-with-memoize sqr 1)
 ]
   (print "Simple integral [FIRST RUN]: ")
-  (time (simpleSqrIntegral 100000))
+  (time (simpleSqrIntegral 100))
   (print "Simple integral [SECOND RUN]: ")
-  (time (simpleSqrIntegral 100000))
+  (time (simpleSqrIntegral 101))
+  (print "Simple integral [THIRD RUN]: ")
+  (time (simpleSqrIntegral 99))
   (print "Memoize integral [FIRST RUN]: ")
-  (time (memoizeSqrIntegral 100000))
+  (time (memoizeSqrIntegral 100))
   (print "Memoize integral [SECOND RUN]: ")
-  (time (memoizeSqrIntegral 100000))
+  (time (memoizeSqrIntegral 101))
+  (print "Memoize integral [THIRD RUN]: ")
+  (time (memoizeSqrIntegral 99))
 )
